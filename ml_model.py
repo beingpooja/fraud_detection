5c0867aae06017e22ba9614203bb36881f5fb491
beingpooja <akudalvi@gmail.com>
2023-09-28 18:32:51 +0530

    added 3 new model

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("Adding all machine related code here")
+
+def model():
+    print("Adding code for Ml model")
+
+def model2():
+    print("Adding code for Ml model2")
+
+def model3():
+    print("Adding code for Ml model3")
